8ee93dc1d9cbf07d818fe7407a12a61a084eb046
nluce <nluce@g.clemson.edu>
2018-06-12 15:18:47 -0400

    new tab link

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
         <div id="titleContainer" class="col-xs-7">
             <h1 style="color: white"><b>Nicholas Luce | Coding Challenge</b></h1>
             <h3 style="color: white">HTML5, CSS, JavaScript, PHP, and MySQL hosted by Heroku.</h3>
-            <h3 style="color: white">Code available <a href="https://github.com/nluce4/WebAppCodingChallenge"><u>here</u></a>.</h3>
+            <h3 style="color: white">Code available <a target="_blank" href="https://github.com/nluce4/WebAppCodingChallenge"><u>here</u></a>.</h3>
         </div>
 
     </div>
