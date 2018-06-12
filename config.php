7776b8f98a6a51c1bf9d820e3b3cf6bba738da38
nluce <nluce@g.clemson.edu>
2018-06-12 15:05:20 -0400

    config update

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-$dbhost = "codingchallenge.c8faxofcl8to.us-east-2.rds.amazonaws.com";
-$dbuser = "nluce";
-$dbpass = "passportChallenge";
-$database = "codingChallenge";
+$dbhost = "qzkp8ry756433yd4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$dbuser = "e298d30x1qr6zsv3";
+$dbpass = "zrsml69lrdgn0f6e";
+$database = "tk7bgjxj68pi42b8";
 ?>
